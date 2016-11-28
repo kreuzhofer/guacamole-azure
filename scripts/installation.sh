17913cd9dcfa7b93ccd5b2d29dfb71508c440ba4
Daniel Kreuzhofer (@dkreuzh) <daniel@kreuzhofer.de>
2016-11-28 19:08:06 +0100

    Create installation.sh

---
+++ scripts/installation.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv \
+               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
