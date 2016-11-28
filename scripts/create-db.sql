855e7571fbc5aa035b8f7c698ce26524751130d9
Daniel Kreuzhofer (@dkreuzh) <daniel@kreuzhofer.de>
2016-11-28 20:24:06 +0100

    Update create-db.sql

--- scripts/create-db.sql
+++ scripts/create-db.sql
@@ -1,4 +1,7 @@
+
 CREATE DATABASE guacamole_db;
 CREATE USER 'guacamole_user'@'localhost' IDENTIFIED BY 'some_password';
 GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'localhost';
+CREATE USER 'guacamole_user'@'%' IDENTIFIED BY 'some_password';
+GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'%';
 FLUSH PRIVILEGES;
