5a1bf6eec4e7116dc67624458fb9159de0bf2153
Daniel Kreuzhofer (@dkreuzh) <daniel@kreuzhofer.de>
2016-11-28 20:07:49 +0100

    Update installation.sh

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -14,3 +14,9 @@ sudo service docker start
 sudo docker run --name some-guacd -d glyptodon/guacd
 sudo docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=PassW0rd! -d mysql:latest
 sudo docker run --rm glyptodon/guacamole /opt/guacamole/bin/initdb.sh --mysql > initdb.sql
+# create database
+curl --url https://raw.githubusercontent.com/kreuzhofer/guacamole-azure/master/scripts/create-db.sql > create-db.sql
+sudo docker cp ./create-db.sql some-mysql:/
+sudo docker cp ./initdb.sql some-mysql:/
+sudo docker exec some-mysql sh -c 'exec mysql -u root -pPassW0rd! < /create-db.sql'
+sudo docker exec some-mysql sh -c 'exec mysql -u root -pPassW0rd! < /initdb.sql'
